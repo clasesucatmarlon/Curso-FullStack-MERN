6e92ec7e94dde8d8faaca3e677af361fa9180421
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-04-12 17:10:14 -0500

    feat(Tickets-server): :sparkles: Estructura para crear un usuario
    Estructura para crear un usuario

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,7 +1,23 @@
+import User from "../models/user.model.js";
+
 export class UserController {
+
+    // CREAR USUARIO
     static createUser = async (req, res) => {
-        const { name } = req.body;
-        res.send(name);
+        const { firstName, lastName, email, password } = req.body;
+        try {
+            const user = new User({ firstName, lastName, email, password })
+            await user.save();  // Guardar en DB
+            res.status(202).json({
+                response: 'success',
+                message: 'Usuario creado',
+            });
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return res
+                .status(500)
+                .json({ response: 'error', message: 'Error del servidor' });
+        }
     }
 
 }
--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 
 import {userRoutes} from "./routes/v1/user.routes.js"
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
+
+// BASE DE DATOS
+connectDB();
 
 // JSON
 app.use(express.json());
