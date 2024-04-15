75a22c256f580ac00c856952a754066197a10f69
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-04-15 10:37:29 -0500

    feat(Tickets-server): :sparkles: Creado endpoint para crear usuario
    Creado endpoint para crear usuario

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,13 +1,59 @@
+import { generateToken } from "../utililies/token.util.js";
+import { hashPassword } from "../utililies/user.util.js";
+import { validateEmail } from "../validators/email.validator.js";
 import User from "../models/user.model.js";
+import Token from "../models/token.model.js";
 
 export class UserController {
 
     // CREAR USUARIO
     static createUser = async (req, res) => {
-        const { firstName, lastName, email, password } = req.body;
         try {
-            const user = new User({ firstName, lastName, email, password })
-            await user.save();  // Guardar en DB
+            const { firstName, lastName, email, password } = req.body;
+
+            // VALIDACIONES
+            if (!firstName || !lastName || !email || !password) {
+                return res.status(400).json({
+                    response: 'error',
+                    message: 'Todos los datos son obligatorios',
+                });
+            }
+            const userExists = await User.findOne({ email });
+            if (userExists) {
+                return res
+                    .status(409)
+                    .json({ response: 'error', message: 'El usuario ya existe' });
+            }
+            if (!validateEmail(email)) {
+                return res
+                    .status(409)
+                    .json({ response: 'error', message: 'Email no válido' });
+            }
+            if (password.length < 6) {
+                return res.status(409).json({
+                    response: 'error',
+                    message: 'El password debe contener al menos 6 caracteres',
+                });
+            }
+
+            // HASHEAR PASSWORD ANDES DE GUARDARLO EN LA BD
+            const hashedPassword = await hashPassword(password);
+
+            // CREAR ESTRUCTURA CON DATA RECIBIDA POR EL BODY
+            const user = new User({ firstName, lastName, email, password: hashedPassword })
+
+            // GENERAR TOKEN DE 6 DIGITOS PARA CONFIRMAR CUENTA
+            const token = new Token({
+                token: generateToken(),
+                user: user._id,
+            });
+
+            // GUARDAR USUARIO y TOKEN EN BD
+            await Promise.allSettled([user.save(), token.save()]);
+
+            // TODO: REGISTRARSE Y MANDAR EMAIL PARA CONFIRMAR REGISTRO 
+
+            // RESPUESTA LUEGO DE GUARDAD EN BD DE MANERA CORRECTA
             res.status(202).json({
                 response: 'success',
                 message: 'Usuario creado',
